5cbeaa67c53c5ab1a263f969ad0d7bec3124a371
roteKlaue <janoolhd@gmail.com>
2024-11-06 20:36:00 +0100

    episode 3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Client, IntentsBitField } from "discord.js";
+import { Client, IntentsBitField, InteractionType, REST, Routes, SlashCommandBuilder } from "discord.js";
 import "dotenv/config";
 
 const client = new Client({
@@ -13,12 +13,48 @@ client.on("messageCreate", (message) => {
     if (message.author.bot) return;
 
     if (message.content === "!ping") {
-        message.reply("Pong!");
+        message.reply({ content: "Pong!" });
     }
 });
 
+client.on("interactionCreate", (action) => {
+    if (action.type !== InteractionType.ApplicationCommand) {
+        return;
+    }
+
+    if (action.commandName === "ping") {
+        action.reply({ content: "Pong!" });
+    }
+ });
+
 client.on("ready", () => {
     console.log(`Bot rennt.`);
+
+    if (process.env.UPDATE_SLASH_COMMANDS === "true") {
+        const rest = new REST({ version: '10' })
+            .setToken(process.env.TOKEN!!);
+
+        const commands = [
+            new SlashCommandBuilder()
+                .setName("ping")
+                .setDescription("Ping Pong command")
+                .toJSON()
+        ];
+
+        /*
+            {
+                "name": string,
+                "description": string,
+                "options": array[ { "name": string, "description": string, "type": "user"|"number"|"string"|"channel", "required": boolean } ]
+            }
+
+            bsp: ban [username]
+        */
+
+        rest.put(Routes.applicationCommands(client.user!.id), { body: commands }).then(e => {
+            console.log(`Slash commands updated.`);
+        });
+    }
 });
 
 client.login(process.env.TOKEN);
