d40f31696d42415301c453541583ad73adecb869
roteKlaue <janoolhd@gmail.com>
2024-11-06 20:08:27 +0100

    episode 2

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello world!");
+import { Client, IntentsBitField } from "discord.js";
+import "dotenv/config";
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.Guilds
+    ]
+});
+
+client.on("messageCreate", (message) => {
+    if (message.author.bot) return;
+
+    if (message.content === "!ping") {
+        message.reply("Pong!");
+    }
+});
+
+client.on("ready", () => {
+    console.log(`Bot rennt.`);
+});
+
+client.login(process.env.TOKEN);
